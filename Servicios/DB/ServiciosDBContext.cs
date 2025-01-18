5812f9a3bd4378afeb7e4890375a7e6db81688a6
StevenJM05 <116135869+StevenJM05@users.noreply.github.com>
2025-01-17 22:27:23 -0600

    Agregar archivos de proyecto.

---
+++ Servicios/DB/ServiciosDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Servicios.DB
+{
+	public class ServiciosDBContext : DbContext
+	{
+		public ServiciosDBContext(DbContextOptions<ServiciosDBContext> options) : base(options) { }
+
+
+	}
+}
